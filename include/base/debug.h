0cd84950822be3c5cf0f479f2397a9c0141a6adf
David Thomas <dave@davespace.co.uk>
2014-12-05 22:35:07 +0000

    base/debug.h: Don't log file:line in info and warning log levels.
    Add an abort log function and use that from new macro haltf(), used for
    'die' type messages.

--- include/base/debug.h
+++ include/base/debug.h
@@ -6,20 +6,23 @@
 #include <stdio.h>
 
 /* Log a message - available in all builds. */
-#define logf_info(fmt, ...)    fprintf(stderr, "(info) %s:%d " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-#define logf_warning(fmt, ...) fprintf(stderr, "(warning) %s:%d " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define logf_info(fmt, ...)    fprintf(stderr, "(info) " fmt "\n", ##__VA_ARGS__)
+#define logf_warning(fmt, ...) fprintf(stderr, "(warning) " fmt "\n", ##__VA_ARGS__)
 #define logf_error(fmt, ...)   fprintf(stderr, "(ERROR) %s:%d " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #define logf_fatal(fmt, ...)   fprintf(stderr, "(FATAL) %s:%d " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define logf_abort(fmt, ...)   fprintf(stderr, "(ABORT) %s:%d " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
 /* Log a debug message - available only in debug builds. */
 #ifndef NDEBUG
-#  define debugf(fmt, ...)     fprintf(stderr, "(debug) %s:%d " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-#  define check(err)           do { if (err) { logf_error("%x", err); goto failure; } } while (0)
+#  define debugf(fmt, ...)     fprintf(stderr, "(debug) " fmt "\n", ##__VA_ARGS__)
+#  define check(err)           do { if (err) { logf_error("err=%x", err); goto failure; } } while (0)
 #  define sentinel             do { logf_error("sentinel"); goto failure; } while (0)
 #else
 #  define debugf(fmt, ...)
 #  define check(err)
 #  define sentinel
 #endif
+
+#define haltf(...)             do { logf_abort(__VA_ARGS__); abort(); } while (0)
 
 #endif /* BASE_DEBUG_H */
