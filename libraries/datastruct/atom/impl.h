dd18e5706010ac8b1f80076f2a82b98d040902ab
David Thomas <dave@davespace.co.uk>
2014-12-15 17:22:25 +0000

    datastruct/atom/impl.h: Add and improve commentary.

--- libraries/datastruct/atom/impl.h
+++ libraries/datastruct/atom/impl.h
@@ -1,5 +1,14 @@
 /* impl.h -- atoms */
 
+/* An atom_set consists of a series of block pools and location pools.
+ *
+ * Block pools are allocated up front and filled with atoms (chunks of data)
+ * until they're full, or run out of room, at which point a new block is
+ * allocated.
+ *
+ * Location pools hold pointers to allocated blocks within the block pools.
+ */
+
 #ifndef IMPL_H
 #define IMPL_H
 
@@ -10,21 +19,33 @@
 #define LOG2LOCPOOLSZ 5 /* default: 32 locs per pool */
 #define LOG2BLKPOOLSZ 9 /* default: 512 bytes per pool */
 
-#define LOCPTRMINSZ 4 /* minimum number of pool pointers to allocate */
-#define BLKPTRMINSZ 4 /* minimum number of pool pointers to allocate */
+#define LOCPTRMINSZ   4 /* minimum number of locpool_t's to allocate */
+#define BLKPTRMINSZ   4 /* minimum number of blkpool_t's to allocate */
 
 /* ----------------------------------------------------------------------- */
 
-/* Convert an atom into a loc. */
+/** 
+ * Convert an atom into a loc. 
+ */
 #define ATOMLOC(i) \
   s->locpools[(i) >> s->log2locpoolsz].locs[(i) & ((1 << s->log2locpoolsz) - 1)]
+
+/**
+ * Retrieve an atom's pointer.
+ */
 #define ATOMPTR(i) \
   (ATOMLOC(i).ptr)
+
+/** 
+ * Retrieve an atom's length.
+ */
 #define ATOMLENGTH(i) \
   (ATOMLOC(i).length)
 
-/* Returns true if the specified atom corresponds to an allocated (but not
- * necessarily used) index. */
+/** 
+ * Returns true if the specified atom corresponds to an allocated (but not
+ * necessarily used) index.
+ */
 #define ATOMVALID(i) \
   ((unsigned int) i < ((s->l_used - 1) << s->log2locpoolsz) + s->locpools[s->l_used - 1].used)
 
@@ -33,24 +54,24 @@
 /* Stores the location and length of a block. */
 typedef struct loc
 {
-  unsigned char *ptr;    /* pointer to block */
-  int            length; /* length of block (-ve if deallocated) */
+  unsigned char  *ptr;    /* pointer into block */
+  int             length; /* length of block (-ve if deallocated) */
 }
 loc_t;
 
 /* Stores the location and used count of a location pool. */
 typedef struct locpool
 {
-  struct loc *locs;
-  int         used;
+  struct loc     *locs;
+  int             used;
 }
 locpool_t;
 
 /* Stores the location and used count of a block pool. */
 typedef struct blkpool
 {
-  unsigned char *blks;
-  int            used;
+  unsigned char  *blks;
+  int             used;
 }
 blkpool_t;
 
