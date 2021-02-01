c9de75374cf4c2792486d0d7436fb40a0c3dc06a
David Thomas <dave@davespace.co.uk>
2021-02-01 22:24:18 +0000

    result.h: Make space for AppEngine's errors

--- include/base/result.h
+++ include/base/result.h
@@ -21,10 +21,12 @@ typedef int result_t;
 #define result_BASE_BITFIFO                     0x0A00
 
 /* Non-DPTLib bases */
-#define result_BASE_MMPLAYER                    0xF000
-#define result_BASE_CONTAINER                   0xF100
-#define result_BASE_CONTAINER_HASH              0xF200
-#define result_BASE_CONTAINER_QUEUE             0xF300
+#define result_BASE_MMPLAYER                    0x4000
+#define result_BASE_CONTAINER                   0x4100
+#define result_BASE_CONTAINER_HASH              0x4200
+#define result_BASE_CONTAINER_QUEUE             0x4300
+
+/* AppEngine uses 0x8000+ */
 
 /* ----------------------------------------------------------------------- */
 
