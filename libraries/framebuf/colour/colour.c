7ff5c3a0b7e918a440cd13041e4c803fe5dd8fe9
David Thomas <dave@davespace.co.uk>
2024-01-25 00:34:07 +0000

    closest_palette_entry: Use weights when colour matching

--- libraries/framebuf/colour/colour.c
+++ libraries/framebuf/colour/colour.c
@@ -24,27 +24,51 @@ static unsigned int closest_palette_entry(const colour_t *palette,
                                           int             nentries,
                                           colour_t        required)
 {
-  int i;
-  int closest = -1;
-  int dist    = INT_MAX;
+#if 1
+  /* libjpeg's weights */
+  const int red_weight   = 19595; /* 0.29900 * 65536 (rounded down) */
+  const int green_weight = 38470; /* 0.58700 * 65536 (rounded up)   */
+  const int blue_weight  =  7471; /* 0.11400 * 65536 (rounded down) */
+#else
+  /* libpng's weights */
+  const int red_weight   = 13938; /* 0.212671 * 65536 (rounded up)   */
+  const int green_weight = 46869; /* 0.715160 * 65536 (rounded up)   */
+  const int blue_weight  =  4729; /* 0.072169 * 65536 (rounded down) */
+#endif
+
+  pixelfmt_rgba8888_t req;
+  int                 closest;
+  unsigned int        dist;
+  int                 req_r, req_g, req_b;
+  int                 i;
+  pixelfmt_rgba8888_t ent;
+  int                 ent_r, ent_g, ent_b;
+  int                 dr, dg, db;
+  unsigned int        curdist;
+
+  req = required.primary;
+  req_r = PIXELFMT_Rxxx8888(req);
+  req_g = PIXELFMT_xGxx8888(req);
+  req_b = PIXELFMT_xxBx8888(req);
+
+  closest = -1;
+  dist    = INT_MAX;
 
   for (i = 0; i < nentries; i++)
   {
-    pixelfmt_rgba8888_t ent = palette[i].primary;
-    pixelfmt_rgba8888_t req = required.primary;
-    int ent_r = PIXELFMT_Rxxx8888(ent);
-    int ent_g = PIXELFMT_xGxx8888(ent);
-    int ent_b = PIXELFMT_xxBx8888(ent);
-    int req_r = PIXELFMT_Rxxx8888(req);
-    int req_g = PIXELFMT_xGxx8888(req);
-    int req_b = PIXELFMT_xxBx8888(req);
-    int dr = ent_r - req_r;
-    int dg = ent_g - req_g;
-    int db = ent_b - req_b;
-    int newdist = (dr * dr) + (dg * dg) + (db * db); // Note: no weights yet
-    if (newdist < dist)
+    ent = palette[i].primary;
+    ent_r = PIXELFMT_Rxxx8888(ent);
+    ent_g = PIXELFMT_xGxx8888(ent);
+    ent_b = PIXELFMT_xxBx8888(ent);
+    dr = ent_r - req_r;
+    dg = ent_g - req_g;
+    db = ent_b - req_b;
+    curdist = ((dr * dr) * red_weight   +
+               (dg * dg) * green_weight +
+               (db * db) * blue_weight) >> 16;
+    if (curdist < dist)
     {
-      dist    = newdist;
+      dist    = curdist;
       closest = i;
     }
   }
